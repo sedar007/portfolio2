15addaa0f35b7970af13ebef3f1349942ad8c08e
Adjy Sedar Desir <desiradjysedar@gmail.com>
2023-01-15 16:07:04 +0100

    ajout download cv

--- JS/main.js
+++ JS/main.js
@@ -161,6 +161,46 @@ function  education(){
     }
 }
 
+function CV(){
+    let button = document.getElementById("btn-cv")
+
+    button.addEventListener('mousedown', function (){
+        DownloadFile("test.pdf");
+    })
+}
+
+
+function DownloadFile(fileName) {
+    //Set the File URL.
+    var url = fileName;
+
+    //Create XMLHTTP Request.
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.responseType = "blob";
+    req.onload = function () {
+        //Convert the Byte Data to BLOB object.
+        var blob = new Blob([req.response], { type: "application/octetstream" });
+
+        //Check the Browser type and download the File.
+        var isIE = false || !!document.documentMode;
+        if (isIE) {
+            window.navigator.msSaveBlob(blob, fileName);
+        } else {
+            var url = window.URL || window.webkitURL;
+            link = url.createObjectURL(blob);
+            var a = document.createElement("a");
+            a.setAttribute("download", fileName);
+            a.setAttribute("href", link);
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+    };
+    req.send();
+};
+
+
 document.addEventListener('DOMContentLoaded', function () {
     addMenu();
     ImagesHome()
@@ -169,6 +209,7 @@ document.addEventListener('DOMContentLoaded', function () {
     hireme()
     desac_education()
     education();
+    CV();
 
 
 })
