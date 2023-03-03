650ff83e84b7c9d4573a1669b631999949b291d8
Adjy Sedar Desir <desiradjysedar@gmail.com>
2023-03-03 21:32:51 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,287 @@
+<!-- Adjy Sedar DÉSIR -->
+
+<!doctype html>
+<html lang="Fr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS responsive avec Bootstrap  -->
+    <link href="css/css.css" rel="stylesheet">
+    <title>Portofolio Sédar</title>      
+  </head>
+
+  <body>
+
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-dark">
+            <div class="container-fluid" >
+                <a class="navbar-brand" href="index.html">
+                    <img src="images/Logos/logo.svg" alt="logo Sedar" width="110" height="50">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav" >
+                  <ul class="navbar-nav">
+                    <li class="nav-item">
+                      <a class="nav-link active" href="index.html" style="color: var(--color_)">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link active" href="#A_Propos" style="color: var(--color_)">A propos</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link active" href="#Compétences" style="color: var(--color_)">Compétences</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link active" href="#Projets" style="color: var(--color_)">projets</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link active" href="#Resume" style="color: var(--color_)" >Résumé</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link active" href="#Contact" style="color: var(--color_)" >Contact</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+        </nav>
+      </header>   
+
+      <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center py-5" style="background-image: url('images/Photos/photo_banner.jpg');" alt="banner">
+        <div class="col-md-5 p-lg-5 mx-auto my-5">
+          <h1 class="display-4 fw-normal" style="color: var(--color_B)">Salut!!</h1>
+          <p class="lead fw-normal" style="color: var(--color_B)" >Salut! Je suis Adjy Sédar DÉSIR<br>
+            Un développeur freelance.
+          </p>
+          <a class="btn btn-outline-secondary" href="#Contact" style="color: var(--color_B)">Contact</a>
+          <a class="btn btn-outline-secondary" href="#Projets" style="color: var(--color_B)">Mes realisations</a>
+          <a class="btn btn-outline-secondary" href="#A_Propos" style="color: var(--color_B)">A propos</a>
+        </div>
+      </div>
+      
+
+      <div class="container-fluid"  id="A_Propos">
+        <div class="container col-xxl-8 px-4 py-5">
+          <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <h1 class="display-5 fw-bold lh-1 mb-3"><center>A propos</center></h1>
+            <div class="col-lg-6">
+              <p class="lead" style="text-align: justify;">Je suis Adjy Sédar DÉSIR, je viens de Port-au-Prince, Haïti. La technologie est ma passion, plus précisément "le codage".
+                J'ai commencer à apprendre à coder avec des tutos sur youtube et des cours gratuits sur "Openclassrooms". Maintenant je suis des cours en licence informatique à l'université du littoral cote d'opale.<br><br>
+                J'ai appris les bases de certains langages de programmation tels que Python, C++.
+                J'ai réalisé des petits programmes comme un jeu de hasard avec tirage d'un nombre au hasard,
+                j'ai aussi réalisé un programme pour donner les racines des nombres du second degré.<br><br>
+                ops ! J'ai presque oublié que j'apprends Html, CSS, comme vous pouvez le deviner car ce site est ma création, <i>J'utilise aussi Bootstrap <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-laughing" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="M12.331 9.5a1 1 0 0 1 0 1A4.998 4.998 0 0 1 8 13a4.998 4.998 0 0 1-4.33-2.5A1 1 0 0 1 4.535 9h6.93a1 1 0 0 1 .866.5zM7 6.5c0 .828-.448 0-1 0s-1 .828-1 0S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 0-1 0s-1 .828-1 0S9.448 5 10 5s1 .672 1 1.5z"/>
+                </svg></i>.Je commence aussi a apprendre PhP et SQL pour faire des connections des sites webs aux serveurs (Page de connection) .<br><br>
+                D'autres passions que la programmation ?<br>
+                
+                Euh !! oui, je joue aussi à des jeux sur console, mobiles, tels que Call of duty, Pubg mobile, FIFA, Brawls Stars.<br>
+                
+                A l'avenir, j'aimerais être spécialiste Data Scientist, travailler dans une grande entreprise comme GOOGLE, puis plus tard avoir ma propre entreprise..
+              </p>
+            </div>
+            <div class="col-10 col-sm-8 col-lg-6">
+              <img src="images/Photos/photo_a_propos.svg" alt="Photo Technologie" class="d-block mx-lg-auto img-fluid"width="700" height="800" >
+              
+            </div>
+            
+          </div>
+        </div>
+      </div>
+
+        
+
+      <div class="container py-5 " id="Compétences">
+        <div class=" py-5">
+          <h1><center>Mes Compétences</h1></center>
+          <div class="row">
+            <div class="col-sm">
+              <p><center><h2>Web Design</h2></center></p>
+              <img src="images/Photos_competences/pc_web_design.svg" alt="Photo pc web design" class="img-thumbnail">       
+            </div>
+            <div class="col-sm">
+              <p><center><h2>Python</h2></center></p>
+              <img src="images/Photos_competences/pc_python.svg" alt="Photo pc python" class="img-thumbnail">       
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm">
+              <p><center><h2>PHP</h2></center></p>
+              <img src="images/Photos_competences/pc_php.svg" alt="Photo pc PHP" class="img-thumbnail">       
+            </div>
+            <div class="col-sm">
+              <p><center><h2>SQL</h2></center></p>
+              <img src="images/Photos_competences/pc_sql.svg" alt="Photo pc SQL" class="img-thumbnail">       
+            </div>
+          </div>
+        </div>
+      </div>
+          
+      <div class="container py-5" id="Projets">
+        <div class="py-5">
+          <h1><center>Mes Projets</h1></center>
+          <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item" data-bs-interval="2000">
+                <a href="http://sedar007.ml" target="_blank"><img src="images/Photos/sedar_website.png" class="d-block w-100" alt="Projet 2"></a>
+              </div>
+        
+              <div class="carousel-item active" data-bs-interval="10000">
+                <a href="http://mt-construction.tk/" target="_blank"> <img src="images/Photos/mt-construction.webp" class="d-block w-100" alt="Projet 1"></a>
+              </div>
+                
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+    </div>
+
+    <div class="container py-5" id="Resume">
+      <div class="py-5">
+          <center><h2 >Résumé</h2></center>
+          <p class="lead" style="text-align: justify;">
+            Je suis Adjy Sédar DÉSIR, j'ai 19 ans, je viens d'Haiti, Je suis en 1ère année en licence informatique à l'université du littoral cote d'opale 
+            sur le site de Calais. 
+            Je suis passionné par le développement d'application, de jeux vidéo, et aussi par la robotique. 
+          </p>
+          <h5>Formation</h5>
+          <p class="lead" style="text-align: justify;">        
+            <ul>
+              <li class="lead" style="text-align: justify;" >Sept 2013 - Aout 2020: Baccalauréat scientifique , "Section Mathématiques et Physique" à l'Institution Saint-Louis de Gonzague, Delmas, Haïti</li>
+              <li class="lead" style="text-align: justify;">Sept 2021 - De nos jours: Licence Informatique à l'ULCO, Calais, France</li>
+            </ul>
+          </p>
+        
+        
+        <div class="container"> 
+          <center><h2 >Présentation de l'ULCO</h2></center>
+            <center>
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/bnqO2tqBhrk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+            </center>
+        </div>
+        <div class="container">
+          <center><h2 >Carte de l'ULCO</h2></center>
+          <center><div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2513.40541183836!2d1.8765820157164503!3d50.95320827954789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dc4080e9a7d7bb%3A0x4473bdca0c4169a9!2sUniversit%C3%A9%20du%20Littoral%20C%C3%B4te%20d&#39;Opale!5e0!3m2!1sen!2sfr!4v1638526292452!5m2!1sen!2sfr" loading="lazy"></iframe>
+          </center>
+        </div>
+      </div>
+    </div>
+
+    <div class="elfsight-app-4fd67bff-f6c3-4b98-bff5-2dfe1fdde820"></div>
+
+    <div class="container-fluid"  id="Contact">
+      <div class="container col-xxl-8 px-4 py-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <h1 class="display-5 fw-bold lh-1 mb-3"><center>Contacts</center></h1>
+          <div class="col-lg-6">       
+            <div class="row">
+              <div class="col-sm">
+                <a class="link-dark" href="tel:+33500000000"> <img src="images/Logos/telephone.svg" alt="icone telephone" width="50" height="50"></a>
+                <p> +33 5 00 00 00 00</p>
+              </div>
+              <div class="col-sm">
+                <a class="link-dark" href="mailto:desiradjysedar20@gmail.com"><img src="images/Logos/mail.svg" alt="icone mail" width="50" height="50"></a>
+                  <p>desiradjysedar20@gmail.com</p>
+              </div>
+              <div class="col-sm">
+                <a class="link-dark" href="https://goo.gl/maps/B1A3bXu3Yh5RvtnMA"  target="_blank"><img src="images/Logos/localisation.svg" alt="icone localisation" width="50" height="50"></a>
+                <p>50 Rue Ferdinand Buisson, 62100 Calais</p>
+              </div>
+                          
+            </div>
+            <p class="lead" >
+              <form id="mon_formulaire">
+                <div class="mb-3">
+                  <label for="nom" class="form-label">Votre  Nom</label>
+                  <input type="text" class="form-control" id="nom" minlength="3" maxlength="50">
+        
+                </div>
+
+                <div class="mb-3">
+                  <label for="mail" class="form-label">Votre adresse mail <span style="color:red;">*</span></label>
+                  <input type="email" class="form-control" id="mail" aria-describedby="emailHelp">
+                  
+                  <span id="erreur_mail"></span>
+                </div>
+
+                <div class="mb-3">
+                  <label for="numero" class="form-label">Votre  numero</label>
+                  <input type="tel" class="form-control" id="numero" size="20" minlength="3" maxlength="14">
+                  <span id="erreur_tel"></span>
+                </div>
+
+                <!-- step="" -->
+                <div class="mb-3">
+                  <label for="budget" class="form-label">Votre  Budget</label>
+                  <input type="range" min="150" max="1000"  value="400" class="slider-input" id="budget" name="Budget">  
+                  <label for="budget" class="form-label">Budget: <label  id="budget-value" class="form-label">100</label> €</label>
+                </div>
+
+                <div class="mb-3">
+                  <textarea cols="30" rows="7" class="form-control" placeholder="Message" id="message" name="message" minlength="3" maxlength="2500" ></textarea>
+                  <span id="erreur_message"></span>
+                </div>
+                
+                <button type="submit" class="btn btn-primary" id =" ">Envoyer</button>
+                
+              </form>
+            </p>
+                       
+          </div>
+          <div class="col-10 col-sm-8 col-lg-6">
+            <img src="images/Photos/photo_contact.jpg"  alt="Photo Contact" class="d-block mx-lg-auto img-fluid"width="700" height="800" >
+          </div>        
+        </div>
+      </div>
+    </div>
+    
+
+    <nav class="bg-dark">
+      <div class="container" >
+        <footer class="py-3 my-4">
+          <ul class="nav justify-content-center pb-3 mb-3">
+            <li class="nav-item"><a href="index.html" style="color: var(--color_)" class="nav-link px-2">Home</a></li>
+            <li class="nav-item"><a href="#Compétences"style="color: var(--color_)"  class="nav-link px-2 ">Compétences</a></li>
+            <li class="nav-item"><a href="#Projets" style="color: var(--color_)" class="nav-link px-2 ">Projets</a></li>
+            <li class="nav-item"><a href="#Contact" style="color: var(--color_)" class="nav-link px-2 ">Contact</a></li>
+            <li class="nav-item"><a href="#A_Propos" style="color: var(--color_)" class="nav-link px-2 ">A propos</a></li>
+            <li class="nav-item"><a href="#Resume"style="color: var(--color_)"  class="nav-link px-2 ">Résumé</a></li>
+          </ul>
+
+          <div class="d-flex justify-content-between py-4 my-4 border-top">
+            <p style="color:var(--color_) ;">&copy; 2021 sedar_007, CSS avec <a href="https://getbootstrap.com/" target="_blank" style="color:var(--color_) ;" class="surl">bootstrap,</a> <a href="https://fr.freepik.com/vecteurs/fond" target="_blank" style="color:var(--color_) ;" class="surl">A Propos/Contact photos créé par pikisuperstar.</a> All rights reserved.</p>
+            <ul class="list-unstyled d-flex">
+              <li class="ms-3"><a class="link-dark" href="tel:+33500000000"><img src="images/Logos/telephone.svg" alt="icone telephone" width="24" height="24" /></a></li>
+              <li class="ms-3"><a class="link-dark" href="mailto:desiradjysedar20@gmail.com"><img src="images/Logos/mail.svg" alt="icone mail" width="24" height="24" /></a></li>
+              <li class="ms-3"><a class="link-dark" href="https://github.com/adjy"  target="_blank"><img src="images/Logos/github.svg" alt="icone github" width="24" height="24" /></a></li>
+              <li class="ms-3"><a class="link-dark" href="https://twitter.com/sedar_007"  target="_blank"><img src="images/Logos/twitter.svg" alt="icone twitter" width="24" height="24" /></a></li>
+              <li class="ms-3"><a class="link-dark" href="https://www.instagram.com/sedar_007/"  target="_blank"><img src="images/Logos/instagram.svg" alt="icone instagram" width="24" height="24" /></a></li>
+            </ul>
+          </div>       
+        </footer>
+      </div>
+    </nav>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="javaScript/bootstrapJavaScript.js" src="javaScript/monJava.js"></script>
+    <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+    <script src="javaScript/monJava.js"></script>
+
+   
+    
+
+   
+  </body>
+</html>
+
